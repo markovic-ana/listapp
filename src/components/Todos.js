d8ede980956586b8677597ad916c272311598ba4
Ana Markovic <markovicana@icloud.com>
2022-01-19 21:50:52 +0100

    css

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -4,8 +4,8 @@ import Todosstyles from './Todos.module.css'
 import ListForm from './ListForm'
 import { BsCheckAll } from 'react-icons/bs'
 import { MdClose } from 'react-icons/md'
-import { MdOutlineEditNote } from 'react-icons/md'
-import { MdOutlineExpandMore } from 'react-icons/md'
+import { BsThreeDots } from 'react-icons/bs'
+import { BsChevronExpand } from 'react-icons/bs'
 
 function Todos({ todos, completeTodo, removeTodo, updateTodo }) {
   const [edit, setEdit] = useState({
@@ -23,26 +23,39 @@ function Todos({ todos, completeTodo, removeTodo, updateTodo }) {
   }
 
   return todos.map((todo, index) => (
-    <div className={Todosstyles.card} key={index}>
+    <div
+      className={todo.isComplete ? Todosstyles.complete : Todosstyles.card}
+      key={index}
+    >
       <div className={Todosstyles.title}>{todo.text}</div>
       <BsCheckAll
         key={todo.id}
         onClick={() => completeTodo(todo.id)}
         className={
-          (todo.isComplete && Todosstyles.complete, Todosstyles.checkIcon)
+          todo.isComplete
+            ? Todosstyles.completeCheckIcon
+            : Todosstyles.checkIcon
         }
       />
-      <MdClose
-        onClick={() => removeTodo(todo.id)}
-        className={Todosstyles.deleteIcon}
-      />
-      <MdOutlineEditNote
+      <BsThreeDots
         onClick={() => setEdit({ id: todo.id, value: todo.text })}
-        className={Todosstyles.editIcon}
+        className={
+          todo.isComplete ? Todosstyles.completeIcon : Todosstyles.editIcon
+        }
       />
       <Link to={`/listitems/${todo.id}`}>
-        <MdOutlineExpandMore className={Todosstyles.expandIcon} />
+        <BsChevronExpand
+          className={
+            todo.isComplete ? Todosstyles.completeIcon : Todosstyles.expandIcon
+          }
+        />
       </Link>
+      <MdClose
+        onClick={() => removeTodo(todo.id)}
+        className={
+          todo.isComplete ? Todosstyles.completeIcon : Todosstyles.deleteIcon
+        }
+      />
     </div>
   ))
 }
--- src/routes/Items.js
+++ src/routes/Items.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import ListItemForm from './ListItemForm'
 import { MdClose } from 'react-icons/md'
 import { MdOutlineEditNote } from 'react-icons/md'
+import Itemsstyles from './Items.module.css'
 
 function Items({ items, completeItem, updateItem, removeItem }) {
   const [edit, setEdit] = useState({
@@ -11,17 +12,28 @@ function Items({ items, completeItem, updateItem, removeItem }) {
 
   return items.map((item, index) => (
     <div
-      className={item.isComplete ? 'todo-row completed' : 'todo-row'}
+      className={item.isComplete ? Itemsstyles.completed : Itemsstyles.row}
       key={index}
     >
-      <div key={item.id} onClick={() => completeItem(item.id)}>
+      <div
+        key={item.id}
+        onClick={() => completeItem(item.id)}
+        className={Itemsstyles.item}
+      >
         {item.text}
       </div>
       <div>
-        <MdClose onClick={() => removeItem(item.id)} />
-      </div>
-      <div>
-        <MdOutlineEditNote />
+        <MdClose
+          onClick={() => removeItem(item.id)}
+          className={
+            item.isComplete ? Itemsstyles.completedIcon : Itemsstyles.deleteIcon
+          }
+        />
+        <MdOutlineEditNote
+          className={
+            item.isComplete ? Itemsstyles.completedIcon : Itemsstyles.editIcon
+          }
+        />
       </div>
     </div>
   ))
